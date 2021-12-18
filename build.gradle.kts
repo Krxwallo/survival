58302f320cef333447220ed45084dcf09d60caeb
Krxwallo <ken.grill@gmx.de>
2021-12-18 14:16:35 +0100

    plugin.yml creation in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,27 @@
 
 group = "de.lookonthebrightsi"
 version = "0.0.2"
+val kspigot = "1.18.0"
+val kutils = "0.0.2"
 
 plugins {
     kotlin("jvm") version "1.6.0"
     kotlin("plugin.serialization") version "1.6.0"
     id("io.papermc.paperweight.userdev") version "1.3.2"
+    id("net.minecrell.plugin-yml.bukkit") version "0.5.1"
+}
+
+bukkit {
+    main = "de.lookonthebrightsi.survival.InternalMainClass"
+    website = "https://github.com/Krxwallo/survival"
+    version = project.version.toString()
+    apiVersion = "1.18"
+    authors = listOf("Krxwallo")
+    libraries = listOf(
+        "org.jetbrains.kotlin:kotlin-reflect:1.6.0",
+        "net.axay:kspigot:$kspigot",
+        "de.hglabor.utils:kutils:$kutils"
+    )
 }
 
 repositories {
@@ -15,8 +31,8 @@ repositories {
 
 dependencies {
     paperDevBundle("1.18.1-R0.1-SNAPSHOT")
-    implementation("net.axay:kspigot:1.18.0")
-    implementation("de.hglabor.utils:kutils:0.0.2")
+    implementation("net.axay:kspigot:$kspigot")
+    implementation("de.hglabor.utils:kutils:$kutils")
     implementation(kotlin("reflect"))
 }
 
