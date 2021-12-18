74ce2156b0b38d0cc1aa26a9b2b3565f41212740
Krxwallo <ken.grill@gmx.de>
2021-12-18 15:50:06 +0100

    remove kotlin serialization + reflect

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-@file:Suppress("PropertyName")
-
 group = "de.lookonthebrightsi"
 version = "0.0.2"
 val kspigot = "1.18.0"
@@ -7,7 +5,6 @@ val kutils = "0.0.2"
 
 plugins {
     kotlin("jvm") version "1.6.0"
-    kotlin("plugin.serialization") version "1.6.0"
     id("io.papermc.paperweight.userdev") version "1.3.2"
     id("net.minecrell.plugin-yml.bukkit") version "0.5.1"
 }
@@ -19,7 +16,6 @@ bukkit {
     apiVersion = "1.18"
     authors = listOf("Krxwallo")
     libraries = listOf(
-        "org.jetbrains.kotlin:kotlin-reflect:1.6.0",
         "net.axay:kspigot:$kspigot",
         "de.hglabor.utils:kutils:$kutils"
     )
@@ -33,7 +29,6 @@ dependencies {
     paperDevBundle("1.18.1-R0.1-SNAPSHOT")
     implementation("net.axay:kspigot:$kspigot")
     implementation("de.hglabor.utils:kutils:$kutils")
-    implementation(kotlin("reflect"))
 }
 
 tasks {
